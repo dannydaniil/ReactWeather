c4b620e5eb13730e42c2b12b732f13424ee71caf
Daniel Daniil <dannyd1997@g.ucla.edu>
2017-04-01 17:17:43 -0700

    prep repo for heroku

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 
-// 
+//
 // var WeatherMessage = React.createClass({
 //   render: function(){
 //
--- server.js
+++ server.js
@@ -2,9 +2,24 @@ var express = require('express');
 
 // Create our app
 var app = express();
+const PORT = proccess.env.port || 3000;
+
+// express middleware
+app.use( function(req, res, next){
+
+  // if http
+  if(req.headers['x-forwarded-proto'] === 'http'){
+
+  next();
+  } else {
+
+    // if not, redirect to http
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function () {
+  console.log('Express server is up on port '+ port);
 });
