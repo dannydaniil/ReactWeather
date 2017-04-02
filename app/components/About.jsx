7a844412e6dff58e92ffb7f810444a9aa75f2e78
Daniel Daniil <dannyd1997@g.ucla.edu>
2017-04-02 14:33:56 -0700

    styled up about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,12 @@ var React = require('react');
 var About = (props)=>{
   return(
     <div>
-    <h3>About</h3>
-    <p>Welcome to the About page.</p>
+    <h1 className="text-center">About</h1>
+    <div className="text-center">
+      <p>This is my first project using React</p>
+      <p>I am also using Foundation for styling</p>
+      <p>You are welcome to take a look at this project's gihub repository over <a href="https://github.com/dannydaniil/ReactWeather">here</a></p>
+    </div>
     </div>
   )
 };
